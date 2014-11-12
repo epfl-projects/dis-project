b770a86141b91b72bce3a61ca4882c691737a4f3
nesta7 <morgan.bruhin@gmail.com>
2014-11-12 09:35:21 +0100

    Merge + start Braitenberg controller

--- webots/controllers/conn_swarm_original/conn_swarm_original.c
+++ webots/controllers/conn_swarm_original/conn_swarm_original.c
@@ -1,4 +1,3 @@
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,7 +20,7 @@
 #define BIAS_SPEED        400 // robot bias speed
 #define MAXSPEED          800 // maximum robot speed
 #define COMM_RADIUS       0.7 // radius of radio communication
-
+#define RANGE		1000 // normalisation of the IR sensors for obstacle avoidance
 /*
   **
   **  Auxiliary
@@ -35,6 +34,7 @@ WbDeviceTag receiverir;
 const char *robot_name;
 
 int distances[NB_SENSORS];    // for sensor readings
+int speed[2];		      // for obstacle avoidance
 
 void setSpeed(int LeftSpeed, int RightSpeed) {
   if (LeftSpeed < -MAXSPEED) {LeftSpeed = -MAXSPEED;}
@@ -42,9 +42,11 @@ void setSpeed(int LeftSpeed, int RightSpeed) {
   if (RightSpeed < -MAXSPEED) {RightSpeed = -MAXSPEED;}
   if (RightSpeed >  MAXSPEED) {RightSpeed =  MAXSPEED;}
 
+
   wb_differential_wheels_set_speed(LeftSpeed,RightSpeed);
 
 }
+
 
 void getSensorValues(int *sensorTable) {
   unsigned int i;
@@ -53,7 +55,8 @@ void getSensorValues(int *sensorTable) {
   }
 }
 
-/*
+
+/* 
   **
   **  E-Puck
   **
@@ -64,6 +67,7 @@ void move(){
   setSpeed(BIAS_SPEED,BIAS_SPEED);
 }
 /* ****************************** RESET ****************************** */
+
 void reset()
 {
   int i;
@@ -104,21 +108,47 @@ void run(){
   move();
 }
 
+
+/***************BRAITENBERG : AVOIDANCE OBSTACLES*********************/
+
+void avoid_obstacle(int* speed,const int* ds_value){
+  
+  int brait_coef[8][2]=
+  {{140, -35}, {110, -15}, {80,-10},{-10, -10},
+     {-15, -10}, {-5, 80}, {-30, 90}, {-20, 160} };
+
+  unsigned int i,j;
+  for (i=0; i<2;i++){
+    speed[i]=BIAS_SPEED;
+    for (j=0; j<8 ; j++){
+      speed[i]+= brait_coef[j][i]*(1.0 - (ds_value[j] / RANGE));
+    }
+  }
+}
+
+
+/******************************** MAIN ******************************/
+
 int main(int argc, char *argv[]) {
 
+  
   /* initialize Webots */
   wb_robot_init();
-
+  
   reset();
-
+  
   /* main loop */
   for (;;) {
-
-    run();
-
-    /* perform a simulation step */
-    wb_robot_step(TIME_STEP);
+  
+    getSensorValues(distances);
+    avoid_obstacle(speed,distances);
+    setSpeed(speed[0], speed[1]);
+    //run();
+  
+  /* perform a simulation step */
+  wb_robot_step(TIME_STEP);
+  
   }
-
+  
   return 0;
 }
