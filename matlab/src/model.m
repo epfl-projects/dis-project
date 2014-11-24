50a24ba9b9e434a8981133ed3875d465259e888a
nesta7 <morgan.bruhin@gmail.com>
2014-11-24 21:09:53 +0100

    model without error but without conservative proprieties for now

--- matlab/src/model.m
+++ matlab/src/model.m
@@ -15,8 +15,11 @@ alpha=5; %should be greater than 2
 N_AF(1:N_rob,1)=0;	% # of robots
 N_F(1:N_rob,1)=0;
 N_F(alpha+2,1)=N_rob;
-N_AC(1:N_rob,1)=0;
-N_C(1:N_rob,1)=0;
+N_AC(1:alpha,1)=0;
+N_C(1:alpha,1)=0;
+N_AC(alpha+1:N_rob,1)=0;
+N_C(alpha+1:N_rob,1)=0;
+
 N_Fbar(:,1)=N_AF + N_F;
 N_Cbar(:,1)=N_AC + N_C;
 
@@ -28,20 +31,23 @@ N_Cbar(:,1)=N_AC + N_C;
 
 Pa=[0.00 0.00 0.01 0.02 0.02 0.03 0.03 0.03 0.05 0.05]';	%collision with another robot
 Pl=[0.00 0.12 0.15 0.17 0.18 0.26 0.28 0.31 0.33 0.33]'; 	%loss of a connection in forward state
-Pg=[0.19 0.22 0.24 0.23 0.22 0.17 0.15 0.13 0.11 0.90]';	%gain of a connection
+Pg=[0.19 0.22 0.24 0.23 0.22 0.17 0.15 0.13 0.11 0.09]';	%gain of a connection
 Pr=[0.28 0.30 0.31 0.29 0.28]';		%recovery of a connection [1:alpha]
 Pf=[0.70 0.58 0.56 0.54 0.52]';	%failure to recover a connection [1:alpha]
-Pla=[0.00 0.05 0.09 0.12 0.15];	%loss of a connection in coherence state [1:alpha]
-
-%NOTE : dimension = dmax rows for 1 column
+Pla=[0.00 0.05 0.09 0.12 0.15]';	%loss of a connection in coherence state [1:alpha]
+%test
+Pr(alpha+1:N_rob)=0;
+Pf(alpha+1:N_rob)=0;
+Pla(alpha+1:N_rob)=0;
+Pla=ones(N_rob,1)-Pr-Pf;
 
 %*************************************%
 %Other simulation parameters
 %*************************************%
 
 TA = 10; %number of timesteps to spend in the avoidance state
-TC = 5; %number of timesteps to spend in the coherence state
-k_end=1000;
+TC = 1; %number of timesteps to spend in the coherence state
+k_end=200;
 
 
 %*************************************%
@@ -53,108 +59,126 @@ while(k<k_end)
 	k++;
 
 
-		% # of robots in Forwar and in Coherence state
-		%~ N_Fbar(i,k)=N_AF(i,k)+N_F(i,k);
-		%~ N_Cbar(i,k)=N_AC(i,k)+N_C(i,k);
+	% # of robots in Forwar and in Coherence state
+	N_F(:,k)=N_Fbar(:,k)-N_AF(:,k);
+	N_C(:,k)=N_Cbar(:,k)-N_AC(:,k);
 
-		N_F(:,k)=N_Fbar(:,k)-N_AF(:,k);
-		N_C(:,k)=N_Cbar(:,k)-N_AC(:,k);
+	%**************************************************************************
+	%Calculation of the number of robots which changed states during timestep k
+	%**************************************************************************
 
-		%********************************************************
-		%Calculation of the probability for the step simulation k
-		%********************************************************
+	%lecture of the following variables
+	% P*N_* means the number of robots of N_* who successfully pass the P* test
+	% # of robots which enter in avoidance from forward state
 
-		% # of robots which enter in avoidance from forward state
-		PaN_F(:,k) = test_prob( Pa, N_F(:,k) );
-		% # of robots which enter in avoidance from coherence state
-		PaN_C(:,k) = test_prob( Pa, N_C(:,k) );
-		% # of robots which fail to recover a connection in coherence state
-		PfN_Cbar(:,k) = test_prob( Pf, N_Cbar(:,k) );
-		PrN_Cbar(:,k) = test_prob( Pr, N_Cbar(:,k) );
-		PgN_Fbar(:,k) = test_prob( Pg, N_Fbar(:,k) );
+	% # of robots which entered in avoidance from forward state
+	PaN_F(:,k) = test_prob( Pa, N_F(:,k) );
+	% # of robots which entered in avoidance from coherence state
+	PaN_C(:,k) = test_prob( Pa, N_C(:,k) );
+	% # of robots which fail to recover / recover / lost a connection from coherence state
+	[PfN_Cbar(:,k) PrN_Cbar(:,k) PlaN_Cbar(:,k) ] = test_prob( Pf, Pr, Pla, N_Cbar(:,k) );
+	% # of robots which gain / lost (/ "the rest") a connection from forward state
+	[PgN_Fbar(:,k) PlN_Fbar(:,k) restN_Fbar] =  test_prob( Pg, Pl , N_Fbar(:,k) );
 
-		PlN_Fbar(:,k) = test_prob(Pl(i+1), N_Fbar(i+1,k));
-		PrN_Cbar(:,k) = test_prob(Pr(i-1), N_Cbar(i-1,k)) ...
-		PgPl_Fbar(:,k) =  test_prob( Pg, Pl , N_Fbar(:,k) )
+	%Check the existence of PN_F(:,k-TA)
+	if k>TA
+		PaNF=PaN_F(:,k-TA);
+		PaNC=PaN_C(:,k-TA);
+	else
+		PaNF=0;
+		PaNC=0;
+	end
 
-		%Check the existence of PN_F(:,k-TA)
-		if k>TA
-			PaNF=PN_F(:,k-TA);
-			PaNC=PN_C(:,k-TA);
-		else, PaNF=0; PaNC=0;
-		end
+	if k<k_end
 
 		N_AF(:,k+1)=N_AF(:,k)+PaN_F(:,k)-PaNF;
 		N_AC(:,k+1)=N_AC(:,k)+PaN_C(:,k)-PaNC;
 
-	%part taking into account the interval for the connectivity sampling (TC)
-	%(sampling every TC timestep)
+		%***********************************************************************%
+		%Simulation for every TC timestep (interval of the connectivity sampling)
+		%***********************************************************************%
 
-	%************* simulation for the forward state ************
-	if mod(k-1,TC)==0 && k<k_end-1
-		%~ N_Fbar(1,(k+1)*TC) = N_Fbar(1,k*TC) + Pf(1) * N_Cbar(1,k*TC) - Pg(1) * N_Fbar(k*TC);
-		%~ N_Cbar(1,(k+1)*TC) = N_Cbar(1,k*TC) + Pla(2) * N_Cbar(2,k*TC) + Pl(2) * N_Fbar(2,k*TC) - ( Pr(1) + Pf(1) ) * N_Cbar(1,k*TC);
+		if mod(k-1,TC)==0
 
-		% # robot which doesn't recover a connection
+			%************* simulation for the forward state ************
 
-
-		%for 0 connection
-		N_Fbar(1,k+1:k+TC) = repmat( N_Fbar(1,k) + PfN_Cbar(1) ...
-			- test_prob(Pg(1), N_Fbar(1,k)), 1, TC );
-
-		%for 1 to alpha-1 connection
-		for i=2:alpha
-
-			N_Fbar(i,k+1:k+TC) = repmat( N_Fbar(i,k) + test_prob(Pg(i-1), N_Fbar(i-1,k)) ...
-				+ PfN_Cbar(i) + test_prob(Pr(i-1), N_Cbar(i-1,k)) ...
-				- test_prob( Pg(i) + Pl(i), N_Fbar(i,k)), 1, TC );
-
-		end
-		%for alpha connections
-		i=alpha+1;
-		N_Fbar(i,k+1:k+TC) = repmat( ...
-			N_Fbar(i,k) ...
-			+ test_prob(Pg(i-1), N_Fbar(i-1,k)) ...
-			+ test_prob(Pl(i+1), N_Fbar(i+1,k)) ...
-			+ test_prob(Pr(i-1), N_Cbar(i-1,k)) ...
-			- test_prob( Pg(i) + Pl(i) , N_Fbar(i,k)), 1, TC );
-		%for alpha+1 to max connections -1 connections
-		for i=alpha+2:dmax-1
-			N_Fbar(i,k+1:k+TC) = repmat( N_Fbar(i,k) + test_prob( Pg(i-1), N_Fbar(i-1,k) ) ...
-				+ test_prob( Pl(i+1), N_Fbar(i+1,k) ) - test_prob( Pg(i) + Pl(i), N_Fbar(i,k)), 1, TC );
-		end
-		%for the full connectivity (N_rob-1 connections)
-		N_Fbar(dmax,k+1:k+TC) = repmat ( N_Fbar(dmax,k) + test_prob(Pg(dmax-1), N_Fbar(dmax-1,k)) ...
-			- test_prob( Pl(dmax), N_Fbar(dmax,k) ), 1, TC );
-
-		%************* simulation for the coherence state ***********
-		%for 0 connection
-		N_Cbar(1,k+1:k+TC) = repmat( ...
-			N_Cbar(1,k) ...
-			+ test_prob( Pla(2), N_Cbar(2,k) ) ...
-			+ test_prob( Pl(2), N_Fbar(2,k) ) ...
-			- test_prob( Pr(1), N_Cbar(1,k) ) ...
-			- PfN_Cbar(1) ...
+			%for 0 connection
+			N_Fbar(1,k+1:k+TC) = repmat( ...
+				N_Fbar(1,k) ...
+				+ PfN_Cbar(1,k) ...
+				- PgN_Fbar(1,k) ...
 			, 1, TC );
-		%for 1 to alpha-2 connections
-		for i=2:alpha-1
-			N_Cbar(i,k+1:k+TC) = repmat( ...
-				N_Cbar(i,k) ...
-				+ Pla(i+1) * N_Cbar(i+1,k) ...
-				+ Pl(i+1) * N_Fbar(i+1,k) ...
-				- ( Pr(i) + Pla(i) ) * N_Cbar(i,k) ...
-				- PfN_Cbar(i) ...
+
+			%for 1 to alpha-1 connection
+			for i=2:alpha
+
+				N_Fbar(i,k+1:k+TC) = repmat( N_Fbar(i,k) ...
+					+ PgN_Fbar(i-1,k) ...
+					+ PfN_Cbar(i,k) ...
+					+ PrN_Cbar(i-1,k) ...
+					- PgN_Fbar(i,k) ...
+					- PlN_Fbar(i,k) ...
 				, 1, TC );
-		end
-		%for alpha-1 connections
-		i=alpha;
-		N_Cbar(i,k+1:k+TC) = repmat( ...
-			N_Cbar(i,k) ...
-			+ test_prob( Pl(alpha), N_Fbar(alpha,k)) ...
-			- test_prob( Pr(i) + Pla(i), N_Cbar(i,k)) ...
-			- PfN_Cbar(i) ...
+
+			end
+
+			%for alpha connections
+			i=alpha+1;
+
+			N_Fbar(i,k+1:k+TC) = repmat( N_Fbar(i,k) ...
+				+ PgN_Fbar(i-1,k) ...
+				+ PlN_Fbar(i+1,k) ...
+				+ PrN_Cbar(i-1,k) ...
+				- PgN_Fbar(i,k) ...
+				- PlN_Fbar(i,k) ...
 			, 1, TC );
-	end
+
+			%for alpha+1 to max connections -1 connections
+			for i=alpha+2:dmax-1
+
+				N_Fbar(i,k+1:k+TC) = repmat( N_Fbar(i,k) ...
+					+ PgN_Fbar(i-1,k) ...
+					+ PlN_Fbar(i+1,k) ...
+					- PgN_Fbar(i,k) ...
+					- PlN_Fbar(i,k) ...
+				, 1, TC );
+
+			end
+			%for the full connectivity (N_rob-1 connections)
+			i=dmax;
+
+			N_Fbar(i,k+1:k+TC) = repmat ( N_Fbar(i,k) ...
+				+ PgN_Fbar(i-1,k) ...
+				- PlN_Fbar(i,k)  ...
+			, 1, TC );
+
+			%************* simulation for the coherence state ***********
+			%for 0 connection
+			N_Cbar(1,k+1:k+TC) = repmat( N_Cbar(1,k) ...
+				+ PlaN_Cbar(2,k) ...
+				+ PlN_Fbar(2,k) ...
+				- PrN_Cbar(1,k) ...
+				- PfN_Cbar(1,k) ...
+				, 1, TC );
+			%for 1 to alpha-2 connections
+			for i=2:alpha-1
+				N_Cbar(i,k+1:k+TC) = repmat( N_Cbar(i,k) ...
+					+ PlaN_Cbar(i+1,k) ...
+					+ PlN_Fbar(i+1,k) ...
+					- PrN_Cbar(i,k) ...
+					- PlaN_Cbar(i,k) ...
+					- PfN_Cbar(i,k) ...
+				, 1, TC );
+			end
+			%for alpha-1 connections
+			i=alpha;
+			N_Cbar(i,k+1:k+TC) = repmat( N_Cbar(i,k) ...
+				+ PlN_Fbar(i+1,k) ...
+				- PrN_Cbar(i,k) ...
+				- PlaN_Cbar(i,k) ...
+				- PfN_Cbar(i) ...
+			, 1, TC );
+		end
 end
 
 %*************************************%
@@ -162,9 +186,15 @@ end
 %*************************************%
 
 figure();
-plot(N_Fbar');
-legend();
+plot(N_Fbar'+N_Cbar');
 figure();
-plot(N_Cbar');
-figure();
-plot((N_Fbar+N_Cbar)');
+plot(N_F'+N_AF'+N_C'+N_AC');
+% figure();
+% plot(N_Fbar');
+% legend();
+% figure();
+% plot(N_Cbar');
+% figure();
+% plot(PlN_Fbar');
+% figure();
+% plot(PlaN_Cbar');
--- matlab/src/test_prob.m
+++ matlab/src/test_prob.m
@@ -1,9 +1,44 @@
 
 %function testing the number of robots which are changing state
-function N_passed = test_prob (P,N)
+function varargout = test_prob (varargin)
 
-  for i=1:size(P)(1)
-    N_passed(i,1)=sum(rand(N(i),1)<P(i));
+if nargin < 2
+  error("not enougth parameters");
+
+elseif nargin == 2
+  P=varargin{1};
+  N=varargin{2};
+    for i=1:size(N)(1)
+      N_passed(i,1)=sum(rand(N(i),1)<P(i));
+    end
+  varargout{1}=N_passed;
+
+  return
+elseif nargin ==4
+  N=varargin{end};
+  for i=1:nargin-1,varargout{i}=0; end
+
+  for i=1:size(N)(1)
+    if N(i) !=0
+      P1=varargin{1}( i, 1 );
+      P2=varargin{2}( i, 1 );
+      P3=varargin{3}( i, 1 );
+      N_rand=rand( N(i), 1 );
+
+      varargout{1}(i,1) = sum( N_rand <= varargin{1}(i) );
+      varargout{2}(i,1) = sum( P1 < N_rand && N_rand <= ( P1 + P2 ) );
+      varargout{3}(i,1) = sum( P1 + P2 < N_rand && N_rand <= ( P1 + P2 + P3 ) );
+      varargout{4}(i,1) = N(i) - ( varargout{1}(i,1)+varargout{2}(i,1)+varargout{3}(i,1) );
+      % for j=2:nargin-1
+      %   a = ( N_rand <= sum( [ varargin{j-1:j} ]' )(i) )
+      %   b = ( sum( [varargin{1:j-1}]' )(i) > N_rand )
+      %   varargout{j}(i,1) = sum( sum( [varargin{1:j-1}]' )(i) < N_rand && N_rand <= sum( [varargin{j-1:j}]' )(i) );
+      % end
+    else
+      for k=1:nargin-1, varargout{k}(i,1) = 0; end
+    end
   end
 
+  return
+end
 endfunction
