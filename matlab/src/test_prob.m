d5607aed44fe8f0acb55019e51e4fc0c4304d105
nesta7 <morgan.bruhin@gmail.com>
2014-11-25 11:47:10 +0100

    function which caluculate the number of state changing robots each step

--- matlab/src/test_prob.m
+++ matlab/src/test_prob.m
@@ -3,7 +3,7 @@
 function varargout = test_prob ( varargin )
 
 N=varargin{end};
-tot=0;
+
 %Tranfigure each probability vector into a matrix
 for k=1:nargin-1
   P(:,k)=varargin{k}(:,1);
@@ -15,14 +15,13 @@ for i=1:size(N)(1) %for each number of connections
   if N(i) !=0 %Only for non zero number of robots
 
     N_rand=rand( N(i), 1 ); %Generate one random number per robot at i connection(s)
-
+    tot=0;
     for k=2:nargin
 
       varargout{k-1}(i,1) = sum( ( ( sum( P(i,1:k-1)) <= N_rand ) + ( N_rand < sum( P(i,1:k) ) ) == 2 ) );
       tot = tot + varargout{k-1}(i,1);
 
     end
-
     varargout{nargin}(i,1) = N(i) - tot; % the rest of the robots
 
   else %meaning that N(i)=0 <=> no robot with i connection(s) so all outputs are equal to zero
