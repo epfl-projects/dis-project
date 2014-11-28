46324acea7b9c64ad2ccf85e8d15552bd686c661
Krishna Sapkota <krsapkota@gmail.com>
2014-11-28 20:12:20 +0100

    some parameters tuned

--- webots/controllers/defines.h
+++ webots/controllers/defines.h
@@ -3,13 +3,13 @@
 
 // Simulation parameters
 #define EXP_TIME          900 // Duration of the experiment (in seconds)
-#define TIME_STEP         50 // Simulation time step (ms), must be a multiple of basicTimeStep in WorldInfo
-#define NUM_ROBOTS        3 // Total number of robots in the scene
+#define TIME_STEP         25 // Simulation time step (ms), must be a multiple of basicTimeStep in WorldInfo
+#define NUM_ROBOTS        5 // Total number of robots in the scene
 // Alpha algorithm
 #define ALPHA                4 // Minimum number of neighbors to maintain
-#define BETA				 1 
-#define MAX_RANDOM_TURN      100 // Maximum value of the random turn (in degrees)
-#define MAX_COHERENCE_TIME   15 // Maximum number of timesteps spent in coherence state
+#define BETA				 2
+#define MAX_RANDOM_TURN      50 // Maximum value of the random turn (in degrees)
+#define MAX_COHERENCE_TIME   20 // Maximum number of timesteps spent in coherence state
 #define MAX_AVOIDANCE_TIME   5 // Maximum number of timesteps spent in avoidance sub-state
 // Communication
 #define COMM_RADIUS           0.7 // Range of radio communication (in meters)
