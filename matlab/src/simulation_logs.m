1c8a20af5e41740002fd40509223411fe770cafc
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-07 15:38:53 +0100

    Use condensed log format in Matlab script

--- matlab/src/simulation_logs.m
+++ matlab/src/simulation_logs.m
@@ -7,12 +7,13 @@
 %   FORWARD_AVOIDANCE = 1,
 %   COHERENCE = 2,
 %   COHERENCE_AVOIDANCE = 3
+NUM_STATES = 4;
 
 logsDirectory = '../../webots/data';
 nRobots = 10;
 alpha = 4;
 % TODO: automatically parse all available CSV files form the directory
-timestamp = 1417595583;
+timestamp = 1417962356;
 
 filename = [logsDirectory, '/simulation-', int2str(nRobots), '-alpha', int2str(alpha), '-', int2str(timestamp), '.csv'];
 
@@ -23,27 +24,26 @@ nTimesteps = length(unique(simulation(:, 1)));
 
 %% Plots
 % Overall repartition of time spent in the different states
-hist(simulation(:, 4));
+averageRobotsPerState = sum(simulation(:, 2:5)) ./ nTimesteps;
+plot(0:3, averageRobotsPerState, '.', 'MarkerSize', 20);
 
 % Main plot: average number of robots having each number of
 % neighbors (one curve for each state)
-robotsPerNeighbors = zeros(nRobots, 3);
-for i = 1:nRobots
-    idx = (simulation(:, 4) == (i - 1));
-    % Forward, coherence, avoidance
-    robotsPerNeighbors(i, 1) = sum(simulation(idx, 3) == 0);
-    % Coherence
-    robotsPerNeighbors(i, 2) = sum(simulation(idx, 3) == 2);
-    % Avoidance
-    robotsPerNeighbors(i, 3) = sum((simulation(idx, 3) == 1) | (simulation(idx, 3) == 3));
+robotsPerNeighbors = zeros(nRobots, NUM_STATES);
+for i = 0:(NUM_STATES-1)
+    % Select lines about this state
+    thisState = simulation(simulation(:, 2) == i, 3:end);
+    robotsPerNeighbors(:, i+1) = sum(thisState, 1)' ./ nTimesteps;
 end;
-% Take the average over the number of timesteps
-robotsPerNeighbors = robotsPerNeighbors / nTimesteps;
-% Last column: sum over states
-robotsPerNeighbors(:, 4) = sum(robotsPerNeighbors, 2);
+% Merge the two (subsumed) avoidance states
+robotsPerNeighbors(:, 2:3) = [robotsPerNeighbors(:, 3), robotsPerNeighbors(:, 2) + robotsPerNeighbors(:, 4)];
+% Total
+robotsPerNeighbors(:, 4) = sum(robotsPerNeighbors(:, 1:3), 2);
+
 
 plot(0:(nRobots-1), robotsPerNeighbors);
 xlabel('Connections (number of neighbors)');
 ylabel('Number of robots');
-axis([0 nRobots 0 max(robotsPerNeighbors(:, 4))+3]);
-legend('Forward state', 'Coherence state', 'Avoidance state', 'Sum');
+axis([0 nRobots 0 max(robotsPerNeighbors(:, 4))+1]);
+legend('Forward', 'Coherence', 'Avoidance', 'Any state');
+
