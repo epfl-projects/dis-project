f42755bfa7b1e2900cca845c38660a3db04ab560
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-10 10:19:20 +0100

    Enhance simulation plot

--- matlab/src/simulation_logs.m
+++ matlab/src/simulation_logs.m
@@ -10,24 +10,29 @@
 %   COHERENCE_AVOIDANCE = 3
 clearvars;
 
-logsDirectory = '../../webots/data';
+%% Configuration
+
+logsDirectory = '../data';
 nStates = 4;
-nRobots = 10;
-alpha = 4;
+nRobots = 40;
+alpha = 10;
 
 % Number of initial timesteps to skip
 % (making sure our system has reached its stable state)
 skipped = 10;
 
+% Symbols for each curve of the plot
+symbols = ['x', '*', 'v', '.'];
+
 pattern = [logsDirectory, '/simulation-', int2str(nRobots), '-alpha', int2str(alpha), '-*.csv'];
 filenames = dir(pattern);
 nExperiments = size(filenames, 1);
 
-% TODO: average over experiments (with error bars)
+%% Handle the stats
+
 stats = zeros(nRobots, nStates, nExperiments);
 for i = 1:nExperiments
     simulation = csvread([logsDirectory, '/', filenames(i).name], skipped + 1, 0);
-    % TODO: use only data starting *after* the steady state has been reached
     nTimesteps = length(unique(simulation(:, 1)));
     stats(:, :, i) = experiment_stats(simulation);
 end;
@@ -36,16 +41,22 @@ averaged = mean(stats, 3);
 errors = std(stats, 0, 3);
 averaged = averaged(:, :, 1);
 
-figure;
-hold on;
-for k = 1:4
-    errorbar(0:(nRobots-1), averaged(:, k), errors(:, k));
+%% Plot
+
+if(nExperiments > 0)
+    figure;
+    hold on;
+
+    errorbar(0:(nRobots-1), averaged(:, 1), errors(:, 1), [symbols(1), '-']);
+    errorbar(0:(alpha-1), averaged(1:alpha, 2), errors(1:alpha, 2), [symbols(2), '-']);
+    errorbar(0:(nRobots-1), averaged(:, 3), errors(:, 3), [symbols(3), '-']);
+    errorbar(0:(nRobots-1), averaged(:, 4), errors(:, 4), [symbols(4), '-']);
+
+    title(['Average over ', int2str(nExperiments), ' experiments']);
+    xlabel('Connections (number of neighbors)');
+    ylabel('Number of robots');
+    axis([0 nRobots 0 max(averaged(:, 4))+1]);
+    legend('Forward', 'Coherence', 'Avoidance', 'Any state');
+else
+    fprintf('No simulation results found for alpha = %d with %d robots.\n', alpha, nRobots);
 end;
-title(['Average over ', int2str(nExperiments), ' experiments']);
-xlabel('Connections (number of neighbors)');
-ylabel('Number of robots');
-axis([0 nRobots 0 max(averaged(:, 4))+1]);
-legend('Forward', 'Coherence', 'Avoidance', 'Any state');
-
-
-
